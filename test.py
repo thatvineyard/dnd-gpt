d961461017bd1842f778b59176cd4c312936bc78
Carl Wingardh <c.a.wingardh@gmail.com>
2023-12-05 23:33:55 +0100

    fixed test

--- test.py
+++ test.py
@@ -4,20 +4,22 @@ from components.utils.cli.args import Args
 from components.utils.cli.cliprint import cli_setup_logging
 from components.utils.cli.envvars import EnvVars
 import json_fix
+from components.utils.engine.settings.settings import EngineSettings
 
 from components.utils.engine.state.sessionhandler import SessionHandler
 
 cli_setup_logging(level=(logging.DEBUG if Args.debug else logging.INFO))
 
-sessionHandler = SessionHandler(EnvVars.SESSION_DIRECTORY)
+engineSettings = EngineSettings()
+
+sessionHandler = SessionHandler(engineSettings)
 sessionHandler.__newSession("funtime")
 sessionHandler.saveSession()
 
 
-sessionHandler2 = SessionHandler(EnvVars.SESSION_DIRECTORY)
+sessionHandler2 = SessionHandler(engineSettings)
 sessionHandler2.__loadSession("funtime")
 
 chatSession = ChatSession(
-    sessionHandler.getCurrentSession(),
     EnvVars.OPENAI_KEY,
 )
